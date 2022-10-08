f285e36f68f2b1ceece0d72a7babed4fb8bc960c
Gabriela Stieler <gabibis.stieler@gmail.com>
2022-10-07 21:15:15 -0300

    Primeiro commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Esse aqui é um teste para ver se o expo funciona no emulador!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
